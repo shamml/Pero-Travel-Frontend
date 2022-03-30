aa9bb8376e43f83a73a8785cea8710ba0f8216cf
rakhmaev <rakhmaev1996@mail.ru>
2022-03-30 13:57:33 +0300

    Подправил верстку HomePage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import Header from './Header';
 import { Route, Routes } from 'react-router-dom';
+import Header from './Header';
 import HomePage from './pages/HomePage';
 import SigninPage from './pages/SigninPage';
 import SignupPage from './pages/SignupPage';
 import Excursions from './pages/Excursions';
+import Tours from './pages/Tours';
+import Profile from './pages/Profile';
 import Footer from './Footer';
+import "./styles.css";
 
 const App = () => {
   return (
@@ -15,8 +18,9 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/tours" element={<Excursions />} />
+        <Route path="/tours" element={<Tours />} />
         <Route path="/excursions" element={<Excursions />} />
+        <Route path="/profile" element={<Profile /> } />
       </Routes>
       <Footer />
     </div>
