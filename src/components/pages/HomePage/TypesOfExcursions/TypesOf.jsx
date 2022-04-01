ef46ee135194552f4a922be4a88ec9aa4a9ed0a6
rakhmaev <rakhmaev1996@mail.ru>
2022-04-01 10:34:01 +0300

    Дополнения в homepage

---
+++ src/components/pages/HomePage/TypesOfExcursions/TypesOf.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styels from "./styles.module.css";
+
+const TypesOf = () => {
+  return (
+    <div className={styels.TypesOf}>
+      <div className={styels.TypesOfContent}>
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TypesOf;
