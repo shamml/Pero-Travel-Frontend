38671c274ab4c0e98125e998d38fe7a3d5c524a5
gataev <email>
2022-04-06 14:27:13 +0300

    Функционалка личного кабинет

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,13 +3,15 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import application from './features/application';
 import admin from './features/admin';
-import tours from './features/tours'
+import tours from './features/tours';
+import user from './features/user';
 
 export const store = createStore(
   combineReducers({
     application,
     admin,
-    tours
+    tours,
+    user,
   }),
   composeWithDevTools(applyMiddleware(thunk)),
 );
