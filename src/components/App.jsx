71350368f15975c0c338f50acb95fd0b5340cd33
rakhmaev <rakhmaev1996@mail.ru>
2022-04-07 16:16:46 +0300

    Внес правки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -64,7 +64,7 @@ const App = () => {
         <Route path="/profile" element={<Profile />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
-      {/*<Footer />*/}
+      <Footer />
     </div>
   ) : (
     <div>
